b3612cdfcebbc58c718956d01ce0512623c0d931
Florian <paypal.xolf@yahoo.com>
2015-05-01 20:04:43 +0200

    Added Javascript console

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
-$.document.ready(function(){
-    console.log('Luckdrum Javascript console');
-})
+var cnlVersion = '0.0.1';
+$( document ).ready(function() {
+    if ($( 'input' ).hasClass('pre-s')){
+
+    }
+    console.log( 'Luckdrum console v' + cnlVersion + ' loaded successfully');
+    console.log( 'Never insert some code you dont know about');
+});
